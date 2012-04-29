fe3c8c28addb2142d8c17050ac0db5d747d41c5d
Hugo Mougard <hugo.mougard+github@gmail.com>
2012-04-29 12:51:55 +0200

    organization

--- 1-10/8.hs
+++ 1-10/8.hs
@@ -1,7 +1,7 @@
 import System.IO
 import Data.Char
 
-main = do n <- readFile "number"
+main = do n <- readFile "8.data"
           let number = map digitToInt.concat.lines $ n
           print.snd $ foldl (\(l, s) c -> let l' = tail l ++ [c]
                                           in  (l', max (product l') s))
