18ce804b792de197ae4edc627fc56cdf21333ec8
Mog <mogzor+git@gmail.com>
2012-09-17 23:02:52 +0200

    53 & 55

---
+++ 51-60/53.hs
@@ -0,0 +1,7 @@
+main = print.length $ [ nCr | n <- [1 .. 100]
+                            , r <- [1 .. n]
+                            , let nCr = fact n `div` (fact r * fact (n - r))
+                            , nCr > 1000000 ]
+
+fact :: Integer -> Integer
+fact n = product [1 .. n]
