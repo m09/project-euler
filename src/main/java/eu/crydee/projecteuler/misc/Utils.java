007136b4ae447c1e5d7bba10d01e6dfe916ae548
Mog <mog@crydee.eu>
2014-03-25 01:49:48 +0900

    problem 8 java

--- src/main/java/eu/crydee/projecteuler/misc/Utils.java
+++ src/main/java/eu/crydee/projecteuler/misc/Utils.java
@@ -1,5 +1,6 @@
 package eu.crydee.projecteuler.misc;
 
+import java.io.InputStream;
 import org.apache.log4j.Logger;
 
 public class Utils {
@@ -9,5 +10,12 @@ public class Utils {
 
     static public char[] splitNumberIntoDigits(Number n) {
         return n.toString().toCharArray();
+    }
+
+    static public InputStream getData(int problem) {
+        return Utils.class.getResourceAsStream(
+                "/eu/crydee/projecteuler/problems/P"
+                + String.valueOf(problem)
+                + ".data");
     }
 }
---
+++ src/main/java/eu/crydee/projecteuler/problems/P8.java
@@ -0,0 +1,46 @@
+package eu.crydee.projecteuler.problems;
+
+import eu.crydee.projecteuler.misc.Utils;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import org.apache.log4j.Logger;
+
+public class P8 implements Problem {
+
+    private static final Logger logger
+            = Logger.getLogger(P8.class.getCanonicalName());
+
+    @Override
+    public String getSolution() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                Utils.getData(8)))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            char[] inputChars = sb.toString().toCharArray();
+            Deque<Integer> window = new LinkedList<>();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < inputChars.length; i++) {
+                if (window.size() == 5) {
+                    window.pop();
+                }
+                window.add(Integer.parseInt(String.valueOf(inputChars[i])));
+                int product = window.stream()
+                        .reduce(1, (a, b) -> a * b, (a, b) -> a * b);
+                if (product > max) {
+                    max = product;
+                }
+            }
+            return String.valueOf(max);
+        } catch (IOException ex) {
+
+        }
+        return "couldn't load the data.";
+    }
+
+}
