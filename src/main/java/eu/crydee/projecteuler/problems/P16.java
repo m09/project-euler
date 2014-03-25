709b1900df9a40304a21e3716d8c5632edda1d3f
Mog <mog@crydee.eu>
2014-03-25 11:03:29 +0900

    problem 16 java

---
+++ src/main/java/eu/crydee/projecteuler/problems/P16.java
@@ -0,0 +1,21 @@
+package eu.crydee.projecteuler.problems;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.log4j.Logger;
+
+public class P16 implements Problem {
+
+    private static final Logger logger
+            = Logger.getLogger(P16.class.getCanonicalName());
+
+    @Override
+    public String getSolution() {
+        char[] chars = BigInteger.valueOf(2).pow(1000).toString().toCharArray();
+        Character[] characters = ArrayUtils.toObject(chars);
+        return String.valueOf(Arrays.stream(characters)
+                .mapToInt(a -> Integer.parseInt(a.toString()))
+                .sum());
+    }
+}
