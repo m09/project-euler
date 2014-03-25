552a20a9d546e55034ad9fb78e33e3a36aef893d
Mog <mog@crydee.eu>
2014-03-25 11:29:50 +0900

    problem 17 java

---
+++ src/main/java/eu/crydee/projecteuler/problems/P17.java
@@ -0,0 +1,32 @@
+package eu.crydee.projecteuler.problems;
+
+import java.util.Arrays;
+import org.apache.log4j.Logger;
+
+public class P17 implements Problem {
+
+    private static final Logger logger
+            = Logger.getLogger(P17.class.getCanonicalName());
+
+    @Override
+    public String getSolution() {
+        int zero2nine = combinedSum("one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine"),
+                ten2nineteen = combinedSum("ten", "eleven", "twelve",
+                        "thirteen", "fourteen", "fifteen", "sixteen",
+                        "seventeen", "eighteen", "nineteen"),
+                tens = combinedSum("twenty", "thirty", "forty", "fifty",
+                        "sixty", "seventy", "eighty", "ninety"),
+                toNinetyNine = zero2nine * 9 + ten2nineteen + tens * 10;
+        return String.valueOf(
+                toNinetyNine * 10
+                + zero2nine * 100
+                + "hundred".length() * 900
+                + "and".length() * 891
+                + "onethousand".length());
+    }
+
+    private int combinedSum(String... strings) {
+        return Arrays.stream(strings).mapToInt(s -> s.length()).sum();
+    }
+}
