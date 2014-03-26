8894c76d0a2b94bf75c3821ba47ec9163f110cee
Mog <mog@crydee.eu>
2014-03-26 09:42:11 +0900

    problem 21 java

--- java/src/main/java/eu/crydee/projecteuler/misc/Divisors.java
+++ java/src/main/java/eu/crydee/projecteuler/misc/Divisors.java
@@ -1,7 +1,9 @@
 package eu.crydee.projecteuler.misc;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.apache.log4j.Logger;
 
 public class Divisors {
@@ -32,7 +34,7 @@ public class Divisors {
         }
         return result;
     }
-    
+
     static public Integer numberOfDivisors(long n) {
         long limit = Math.round(Math.sqrt(n));
         int total = 0;
@@ -43,6 +45,42 @@ public class Divisors {
         }
         if (limit * limit == n) {
             total += 1;
+        }
+        return total;
+    }
+
+    static public Set<Long> properDivisors(long n) {
+        long limit = Math.round(Math.sqrt(n));
+        Set<Long> result = new HashSet<>();
+        for (long i = 1; i <= limit; i++) {
+            if (n % i == 0) {
+                result.add(i);
+                result.add(n / i);
+            }
+        }
+        return result;
+    }
+
+    static public long sumProperDivisors(long n) {
+        if (n == 0) {
+            return Long.MAX_VALUE;
+        }
+        if (n == 1) {
+            return 0;
+        }
+        if (n < 0) {
+            n = -n;
+        }
+        long limit = Math.round(Math.sqrt(n)),
+                total = 1;
+        for (long i = 2; i < limit; i++) {
+            if (n % i == 0) {
+                total += i;
+                total += n / i;
+            }
+        }
+        if (limit * limit == n) {
+            total += limit;
         }
         return total;
     }
---
+++ java/src/main/java/eu/crydee/projecteuler/problems/P21.java
@@ -0,0 +1,26 @@
+package eu.crydee.projecteuler.problems;
+
+import eu.crydee.projecteuler.misc.Divisors;
+import org.apache.log4j.Logger;
+
+public class P21 implements Problem {
+
+    private static final Logger logger
+            = Logger.getLogger(P21.class.getCanonicalName());
+
+    @Override
+    public String getSolution() {
+        int total = 0;
+        for (int i = 1; i <= 10000; i++) {
+            long sumDivisors = Divisors.sumProperDivisors(i);
+            if (sumDivisors > i
+                    && Divisors.sumProperDivisors(sumDivisors) == i) {
+                total += i;
+                if (sumDivisors <= 10000) {
+                    total += sumDivisors;
+                }
+            }
+        }
+        return String.valueOf(total);
+    }
+}
