5087f8976d650b1ac8eb3e7e3b3ca698b3700d1c
Mog <mog@crydee.eu>
2014-03-29 00:30:28 +0900

    problem 26 java

--- java/src/main/java/eu/crydee/projecteuler/misc/Utils.java
+++ java/src/main/java/eu/crydee/projecteuler/misc/Utils.java
@@ -2,6 +2,12 @@ package eu.crydee.projecteuler.misc;
 
 import eu.crydee.projecteuler.problems.Problem;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.log4j.Logger;
 
 public class Utils {
@@ -11,6 +17,23 @@ public class Utils {
 
     static public char[] splitNumberIntoDigits(Number n) {
         return n.toString().toCharArray();
+    }
+
+    static public List<Integer> getDigitsList(long n) {
+        return Arrays.stream(
+                ArrayUtils.toObject(String.valueOf(n).toCharArray()))
+                .map(String::valueOf)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    static public Deque<Integer> getDigitsStack(long n) {
+        Deque<Integer> result = new LinkedList<>();
+        char[] chars = String.valueOf(n).toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            result.push(Integer.parseInt(String.valueOf(chars[i])));
+        }
+        return result;
     }
 
     static public int numberOfDigits(Number n) {
---
+++ java/src/main/java/eu/crydee/projecteuler/problems/_21to30/P26.java
@@ -0,0 +1,59 @@
+package eu.crydee.projecteuler.problems._21to30;
+
+import eu.crydee.projecteuler.misc.Utils;
+import eu.crydee.projecteuler.problems.Problem;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.apache.log4j.Logger;
+
+public class P26 implements Problem {
+
+    private static final Logger logger
+            = Logger.getLogger(P26.class.getCanonicalName());
+
+    @Override
+    public String getSolution() {
+        int maxPeriod = Integer.MIN_VALUE,
+                max = -1,
+                period;
+        for (int i = 1; i <= 1000; i++) {
+            if ((period = periodLength(1, i)) > maxPeriod) {
+                max = i;
+                maxPeriod = period;
+            }
+        }
+        return String.valueOf(max);
+    }
+
+    private int periodLength(long numerator, long denominator) {
+        Map<Long, Integer> seen = new HashMap<>();
+        Deque<Integer> digitsNumerator = Utils.getDigitsStack(numerator);
+        int step = 0;
+        for (; !seen.containsKey(numerator); step++) {
+            seen.put(numerator, step);
+            numerator = 0;
+            for (int i = 0; numerator < denominator; i++) {
+                numerator *= 10;
+                if (i < digitsNumerator.size()) {
+                    numerator += digitsNumerator.pop();
+                }
+            }
+            logger.debug("numerator:" + numerator);
+            numerator %= denominator;
+            digitsNumerator.push((int) numerator);
+            logger.debug("new numerator: " + numerator);
+            logger.debug("step: " + step);
+            logger.debug("seen: " + seen);
+            logger.debug("numerator: " + numerator);
+            logger.debug("step: " + step);
+            logger.debug("===");
+            if (numerator == 0) {
+                return 0;
+            }
+        }
+        return step - seen.get(numerator);
+    }
+}
