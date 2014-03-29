c14add25d19b499a9ad083e36080b2bf6845fedd
Mog <mog@crydee.eu>
2014-03-29 14:15:25 +0900

    problem 27

--- java/src/main/java/eu/crydee/projecteuler/problems/_21to30/P27.java
+++ java/src/main/java/eu/crydee/projecteuler/problems/_21to30/P27.java
@@ -1,56 +1,34 @@
 package eu.crydee.projecteuler.problems._21to30;
 
-import eu.crydee.projecteuler.misc.Utils;
+import eu.crydee.projecteuler.primes.Primes;
 import eu.crydee.projecteuler.problems.Problem;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.log4j.Logger;
 
-public class P26 implements Problem {
+public class P27 implements Problem {
 
     private static final Logger logger
-            = Logger.getLogger(P26.class.getCanonicalName());
+            = Logger.getLogger(P27.class.getCanonicalName());
 
     @Override
     public String getSolution() {
-        int maxPeriod = Integer.MIN_VALUE,
-                max = -1,
-                period;
-        for (int i = 1; i <= 1000; i++) {
-            if ((period = periodLength(1, i)) > maxPeriod) {
-                max = i;
-                maxPeriod = period;
-            }
-        }
-        return String.valueOf(max);
-    }
-
-    private int periodLength(long numerator, long denominator) {
-        Map<Long, Integer> seen = new HashMap<>();
-        Deque<Integer> digitsNumerator = Utils.getDigitsStack(numerator);
-        int step = 0;
-        for (; !seen.containsKey(numerator); step++) {
-            seen.put(numerator, step);
-            numerator = 0;
-            for (int i = 0; numerator < denominator; i++) {
-                numerator *= 10;
-                if (i < digitsNumerator.size()) {
-                    numerator += digitsNumerator.pop();
+        int max = Integer.MIN_VALUE;
+        long maxA = -1001;
+        long maxB = -1001;
+        for (long a = -999; a < 1000; a++) {
+            for (long b = -999; b < 1000; b++) {
+                long n = 0l;
+                int steps = 0;
+                while (Primes.isPrime(n * n + a * n + b)) {
+                    steps++;
+                    n++;
+                }
+                if (steps > max) {
+                    max = steps;
+                    maxA = a;
+                    maxB = b;
                 }
             }
-            logger.debug("numerator:" + numerator);
-            if ((numerator %= denominator) == 0) {
-                return 0;
-            }
-            digitsNumerator.push((int) numerator);
-            logger.debug("new numerator: " + numerator);
-            logger.debug("step: " + step);
-            logger.debug("seen: " + seen);
-            logger.debug("numerator: " + numerator);
-            logger.debug("step: " + step);
-            logger.debug("===");
         }
-        return step - seen.get(numerator);
+        return String.valueOf(maxA * maxB);
     }
 }
--- java/src/test/java/eu/crydee/projecteuler/problems/_21to30/P27Test.java
+++ java/src/test/java/eu/crydee/projecteuler/problems/_21to30/P27Test.java
@@ -3,10 +3,10 @@ package eu.crydee.projecteuler.problems._21to30;
 import org.junit.Test;
 import org.junit.Assert;
 
-public class P26Test {
+public class P27Test {
 
     @Test
     public void testGetSolution() {
-        Assert.assertEquals("983", new P26().getSolution());
+        Assert.assertEquals("-59231", new P27().getSolution());
     }
 }
