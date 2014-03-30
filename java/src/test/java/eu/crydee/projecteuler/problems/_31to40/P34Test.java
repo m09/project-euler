f94038201e1db4688c2d436ae6f7f9771bb6a0f1
Mog <mog@crydee.eu>
2014-03-30 14:57:18 +0900

    problem 34

---
+++ java/src/test/java/eu/crydee/projecteuler/problems/_31to40/P34Test.java
@@ -0,0 +1,12 @@
+package eu.crydee.projecteuler.problems._31to40;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class P34Test {
+
+    @Test
+    public void testGetSolution() {
+        Assert.assertEquals("40730", new P34().getSolution());
+    }
+}
