b406445fecf86119ec00414b0a3deb3cd8266454
Mog <mog@crydee.eu>
2014-03-31 19:55:51 +0900

    problem 40

---
+++ java/src/test/java/eu/crydee/projecteuler/problems/_31to40/P40Test.java
@@ -0,0 +1,12 @@
+package eu.crydee.projecteuler.problems._31to40;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class P40Test {
+
+    @Test
+    public void testGetSolution() {
+        Assert.assertEquals("210", new P40().getSolution());
+    }
+}
