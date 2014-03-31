252870a3363065bdd60ba5630c57470281fa42d0
Mog <mog@crydee.eu>
2014-03-31 14:48:04 +0900

    problem 39

---
+++ java/src/test/java/eu/crydee/projecteuler/problems/_31to40/P39Test.java
@@ -0,0 +1,12 @@
+package eu.crydee.projecteuler.problems._31to40;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class P39Test {
+
+    @Test
+    public void testGetSolution() {
+        Assert.assertEquals("840", new P39().getSolution());
+    }
+}
